4e6e3df3a47b26f2be9bfe23922ae615de1c4070
Cynthia dos programa toppp <133544879+cynthprogramer@users.noreply.github.com>
2023-12-05 08:32:55 -0300

    mesma coisa
    -so comentei os coiso

--- jogo.py
+++ jogo.py
@@ -10,9 +10,9 @@ class configuração():
         self.master = master
         self.root = root
         self.tela()
-        root.mainloop()
+        root.mainloop() 
 
-    def tela(self):
+    def tela(self): #nome da janela
         self.root.title('DORA CARNICEIRA 0.2')
         self.root.geometry('1076x717')
         self.root.resizable(True,True)
@@ -37,17 +37,17 @@ def janela02():
         os.execl(jogo,jogo, * sys.argv)
 
 #imagem do menu
-imagem_menu = PhotoImage( file="imagens_do_tkinter/Dora carniceiraa.png", master=root)
-imagem__menu = Label(root, image=imagem_menu).pack()
+imagem_menu = PhotoImage( file="imagens_do_tkinter/Dora carniceiraa.png", master=root) 
+imagem__menu = Label(root, image=imagem_menu).pack() # a dora grandona por toda tela
 
 #botao do menu (start)
 imagem_start = PhotoImage(file='imagens_do_tkinter/start.png')
-imagem_start = imagem_start.subsample(1,1)
-botao_start = Label(root, image=imagem_start)
-botao_start.image = imagem_start
+imagem_start = imagem_start.subsample(1,1) # o tamanho da imagem
+botao_start = Label(root, image=imagem_start) 
+botao_start.image = imagem_start #transformou a imagem em botao
 
 botao_menu = Button (root,image=imagem_start, command = janela02)
-botao_menu.place(x = 200, y = 500)
+botao_menu.place(x = 200, y = 500) # coordenadas de onde o botao ta
 
 
 configuração()
